4ca13eb2a2b4dda071e21b35577b78873b053b1a
xatreyux <mwmailing@gmail.com>
2021-06-29 03:54:54 -0400

    deploying, fixing axios cookie bug

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,6 +1,5 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
   : // "https://us-central1-devport-express-backend.cloudfunctions.net/app"
-    process.env.NODE_ENV === "production" &&
-    "https://devport-express-backend.web.app/";
+    process.env.NODE_ENV === "production" && "https://folioback.web.app";
 // "https://auth-skeleton-backend.herokuapp.com";
